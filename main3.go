3851fee66556e101d69a741f9c243f3d08fe0ccd
LRCSlonik <167251257+LRCSlonik@users.noreply.github.com>
2024-05-29 22:10:33 +0300

    1

--- main3.go
+++ main3.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println("Строковый калькулятор")
+	fmt.Println("Введите выражение")
+
+	var a string
+	var b string
+	var c string
+	fmt.Scan(&a, &b, &c)
+
+	//if b != +
+	//fmt.Println("Ошибка ввода используйте (+, -, *, /)")
+
+	switch {
+	case b == "+":
+		fmt.Println(a + c)
+	case b == "-":
+		fmt.Println(a - c)
+
+	}
 }
